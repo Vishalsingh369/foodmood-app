2510cbc5db994d85fa894d6f005a3994b904abd1
Vishal Thakur <vishalthakur0378958@gmail.com>
2024-03-14 19:55:26 +0530

    episode 2 and 3

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React Series 🚀
+
+# Parcel
+- Dev-build
+- Local server;
+- HMR- hot module replacement
+- file watching algorithm written in c++;
+- Caching - faster builds
+- Image Optimisation
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // const heading = React.createElement("h1", {id: "heading",xyz:"abc"}, "Hello world from react!");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am an h1 tag"),
-    React.createElement("h2", {}, "i am an h2 tag"),
-  ])
+// const heading=React.createElement("h1",{id: "heading"},"Namaste React");
+
+// Babel is converting JSX code to react code
+// Babel is javascript compiler
+
+// jsx= Babel transpiles it to React.createElement => ReactElement JS object => HTMLElement(render)
+
+const jsxheading = (
+  <h1 className="main" tabIndex="1">
+    Namaste react heading
+  </h1>
 );
-
-console.log(parent);
+console.log(jsxheading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxheading);
