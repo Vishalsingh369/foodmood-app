82fe84c0fec2190d505efb51cb88264e9d76e0dc
Vishal Thakur <vishalthakur0378958@gmail.com>
2024-03-15 19:15:06 +0530

    day 2 learning work

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const heading = React.createElement("h1", {id: "heading",xyz:"abc"}, "Hello world from react!");
-import React from "react";
+import React, { Component } from "react";
+// import { render } from "react-dom";
 import ReactDOM from "react-dom/client";
 
 // const heading=React.createElement("h1",{id: "heading"},"Namaste React");
@@ -9,11 +10,117 @@ import ReactDOM from "react-dom/client";
 
 // jsx= Babel transpiles it to React.createElement => ReactElement JS object => HTMLElement(render)
 
-const jsxheading = (
-  <h1 className="main" tabIndex="1">
-    Namaste react heading
-  </h1>
-);
-console.log(jsxheading);
+// const elem=( <h1>namaste world to me</h1>)
+// // this is react element {
+// const jsxheading = (
+//   <h1 className="main" tabIndex="1">
+//     {elem}
+//     Namaste react heading
+//   </h1>
+// );
+// // }
+
+// // React functional based component
+// // and class based Component
+
+// // note: react Component always starts with capital letters
+
+// // const Heading3 = function() { return( <h1>namaste reactjs</h1>)};\
+
+// const number = 100;
+
+// const Heading3 = () => (
+//   <div id="head">
+//     {jsxheading}
+//     {number}
+//     <h1>namaste reactjs</h1>
+//   </div>
+// );
+
+// const Heading1 = () => {
+//   return (
+//     <div>
+//       <h2>{number}</h2>
+//       <h1>Namaste React to the world</h1>
+//     </div>
+//   );
+// };
+
+// // this is Component compositon {
+
+// const Heading2 = () => (
+//   <div>
+//     {/* {
+//     {Heading1()}
+//     <Heading1></Heading1>               this three things are same thing
+//     <Heading1 />
+//   } */}
+//     <h1>Namaste react DOM </h1>
+//     <Heading3 />
+//   </div>
+// );
+// // }
+
+// console.log(jsxheading);
+
+// // react elements rendered like this
+// // root.render(jsxheading);
+
+// // react components rendered like this
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo container">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdrQ7Ly9pqZ6F06bzC5H8hu1Bl7scJF-1IGw&usqp=CAU"
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Contact Us</li>
+          <li>About Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+const ResCard = () => {
+  return (
+    <div className="res-card" style={{
+      backgroundColor:"#f0f0f0"
+    }}>
+      <img className="res-img"
+      src="https://cdn.britannica.com/35/225835-050-A5CC289A/Indian-one-pot-meal-for-party.jpg" alt="res-logo"/>
+      <h3>Meghana Foods</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search Bar</div>
+      <div className="res-container">
+        <ResCard />
+      </div>
+    </div>
+  );
+};
+
+const Applayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<Applayout />);
